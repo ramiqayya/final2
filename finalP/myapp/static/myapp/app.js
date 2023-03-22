4abdf62ab492fb1f088c737e148afc7a772cc006
ramiqayya <rami.qayya@gmail.com>
2023-03-22 23:11:41 +0200

    modify styles using javascript

--- finalP/myapp/static/myapp/app.js
+++ finalP/myapp/static/myapp/app.js
@@ -3,7 +3,21 @@ document.addEventListener("DOMContentLoaded", function () {
     const results = document.querySelector(".results")
     const amount = document.querySelector("#amount")
     const price = document.querySelector("#coinPrice")
+    const message = document.querySelector("#tmessage")
     amount.addEventListener("input", (event) => {
-        results.innerHTML = (event.target.value) * price.innerHTML
+        if (event.target.value >= 0) {
+            results.style.display = "inline"
+            message.style.display = "none"
+            results.innerHTML = (event.target.value) * price.innerHTML
+
+
+        } else if (event.target.value < 0) {
+            results.style.display = "none"
+            message.style.display = "block"
+            message.innerHTML = "Please Enter a Positive Amount"
+            message.style.color = "red"
+        }
+
     })
+
 })
