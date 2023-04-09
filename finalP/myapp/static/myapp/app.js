23bf8477c42dd22225a0b69d0206368c929a3295
ramiqayya <rami.qayya@gmail.com>
2023-04-10 00:15:20 +0300

    add arrows to the tables

--- finalP/myapp/static/myapp/app.js
+++ finalP/myapp/static/myapp/app.js
@@ -3,6 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
     // Select all cells containing percentage changes
     const cells = document.querySelectorAll('td:nth-child(4), td:nth-child(5), td:nth-child(6)');
 
+    // Define the arrow characters
+    const upArrow = '\u2B9d'; // ↑
+    const downArrow = '\u2B9f'; // ↓
+
     // Loop through the cells and format them accordingly
     cells.forEach(cell => {
         const value = parseFloat(cell.textContent);
@@ -10,19 +14,58 @@ document.addEventListener("DOMContentLoaded", function () {
         // If the value is negative, remove the negative sign and color it red
         if (value < 0) {
             cell.style.color = 'red';
-            cell.textContent = `${-value}%`;
+            cell.textContent = `${-value}% ${downArrow}`;
         }
         // If the value is positive, color it green
         else if (value > 0) {
             cell.style.color = 'green';
-            cell.textContent = `${value}%`;
+            cell.textContent = `${value}% ${upArrow}`;
         }
         // If the value is zero, color it gray
         else {
             cell.style.color = 'gray';
-            cell.textContent = '0%';
+            cell.textContent = `0%`;
         }
+
+        // Add the arrow character to the cell's content
+        cell.style.position = 'relative';
+        cell.style.paddingRight = '10px'; // Adjust the padding to make space for the arrow
+        const arrow = document.createElement('span');
+        arrow.style.position = 'absolute';
+        arrow.style.top = '50%';
+        arrow.style.transform = 'translateY(-50%)';
+        arrow.style.right = '0';
+        arrow.style.fontSize = '0.8em'; // Adjust the font size of the arrow
+        arrow.textContent = value < 0 ? downArrow : upArrow;
+        cell.appendChild(arrow);
     });
+
+
+
+    //////////////
+    // Select all cells containing percentage changes
+    // const cells = document.querySelectorAll('td:nth-child(4), td:nth-child(5), td:nth-child(6)');
+
+    // // Loop through the cells and format them accordingly
+    // cells.forEach(cell => {
+    //     const value = parseFloat(cell.textContent);
+
+    //     // If the value is negative, remove the negative sign and color it red
+    //     if (value < 0) {
+    //         cell.style.color = 'red';
+    //         cell.textContent = `${-value}%`;
+    //     }
+    //     // If the value is positive, color it green
+    //     else if (value > 0) {
+    //         cell.style.color = 'green';
+    //         cell.textContent = `${value}%`;
+    //     }
+    //     // If the value is zero, color it gray
+    //     else {
+    //         cell.style.color = 'gray';
+    //         cell.textContent = '0%';
+    //     }
+    // });
 
 
 
